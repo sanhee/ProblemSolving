e2022f28259a6e0225f5f82c28ff426b9140c9bc
noel <68936@naver.com>
2020-12-22 21:40:33 +0900

    백준 2739 구구단 풀이

---
+++ BOJ/_2739/JAVA/BOJ_2739.java
@@ -0,0 +1,20 @@
+package com.example.BOJ._2739.JAVA;
+
+import java.util.Scanner;
+
+public class BOJ_2739 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        for(int i=1;i<10;i++){
+
+            System.out.println(num + " * " + i + " = "+(num*i));
+
+        }
+
+        sc.close();
+    }
+}

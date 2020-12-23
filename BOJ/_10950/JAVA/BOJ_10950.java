0471554bfcedebaf1f098d0db919cab37e0fe253
noel <68936@naver.com>
2020-12-23 16:36:35 +0900

    백준 A+B - 3

---
+++ BOJ/_10950/JAVA/BOJ_10950.java
@@ -0,0 +1,17 @@
+package com.example.BOJ._10950.JAVA;
+
+import java.util.Scanner;
+
+public class BOJ_10950 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int TestCaseNum = sc.nextInt();
+
+        for(int i=0 ; i<TestCaseNum;i++){
+            sb.append(sc.nextInt()+sc.nextInt()).append("\n");
+        }
+        System.out.println(sb);
+    }
+}

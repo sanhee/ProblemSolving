f9e285551b73ae39f762fda06d546ad936c7c5fd
noel <68936@naver.com>
2020-12-26 18:31:20 +0900

    BOJ_10951 EOF예제 문제 풀이

--- BOJ/_10951/JAVA/BOJ_10951.java
+++ BOJ/_10951/JAVA/BOJ_10951.java
@@ -1,22 +1,19 @@
 package com.example.BOJ._10951.JAVA;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class BOJ_10951 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
 
-        while(br.readLine() != null){
-
-            int a = br.readLine().charAt(0)-'0';
-            int b = br.readLine().charAt(2)-'0';
+        while(sc.hasNextInt()){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
             sb.append(a+b).append("\n");
         }
         System.out.println(sb);
-
+        sc.close();
     }
 }

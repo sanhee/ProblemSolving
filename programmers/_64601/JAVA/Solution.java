a81bcafc6125e6b62e5a2dbea1cff2a35cfe2cca
noel <68936@naver.com>
2021-01-21 08:16:59 +0900

    성공

--- programmers/_64601/JAVA/Solution.java
+++ programmers/_64601/JAVA/Solution.java
@@ -1,53 +1,74 @@
 package com.example.programmers._64601.JAVA;
 
-import java.util.Stack;
-
-
-//m 1시간 08분 43초 걸림.
-
 public class Solution {
 
-    public int solution(int[][] board, int[] moves) {
-        int answer = 0;
+    private int checkAnswer(int[] ANSWER_PATTERN, int[] answers){
 
-        int moveLength = moves.length;
-        int yLen = board.length;
-        Stack<Integer> stack = new Stack<>();
+        int patternLen = ANSWER_PATTERN.length;
+        int countOfTest= 0;
+        int loopMax = 0;
+        int numOfTest = answers.length; // 문제 문항 체크
 
-        for(int m=0; m<moveLength; m++){
-
-            int selectPos = moves[m]-1;
-
-            for(int y=0;y<yLen;y++){
-
-                if(board[y][selectPos] > 0) { //m 값이 존재할 경우
-
-                    if(!stack.empty() && (stack.lastElement() == board[y][selectPos])){
-                        stack.pop();
-                        answer +=2;
-                    }
-                    else {
-                        stack.push(board[y][selectPos]);
-                    }
-                    board[y][selectPos] = 0;
-
-                    break;
-                }
-            }
+        if(patternLen < numOfTest){
+            loopMax = patternLen + numOfTest - patternLen;
+        }else{
+            loopMax = numOfTest;
         }
 
-        return answer;
+        int loopCnt = 0;
+        int calibrationRange = 0;
+
+        for(int i=0; i<loopMax;i++){
+            if(loopCnt == loopMax) {
+                break;
+            }
+            if(answers[i] == ANSWER_PATTERN[calibrationRange]){
+                countOfTest++;
+            }
+
+            if(calibrationRange+1 >= patternLen) {
+                calibrationRange = 0;
+            }
+            else{
+                calibrationRange++;
+            }
+            loopCnt++;
+        }
+
+        return  countOfTest;
+
+    }
+
+    public int[] solution(int[] answers) {
+
+        final int[] ANSWER_PATTERN_1 = {1, 2, 3, 4, 5};
+        final int[] ANSWER_PATTERN_2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        final int[] ANSWER_PATTERN_3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+
+        int[] countOfTest = {checkAnswer(ANSWER_PATTERN_1, answers), checkAnswer(ANSWER_PATTERN_2, answers), checkAnswer(ANSWER_PATTERN_3, answers)};
+
+        int max = countOfTest[0];
+
+        if(max<countOfTest[1]) max = countOfTest[1];
+        if(max<countOfTest[2]) max = countOfTest[2];
+
+        if(max == countOfTest[0] && max ==countOfTest[1] && max == countOfTest[2])  return new int[]{1,2,3};
+        else if(max == countOfTest[0] && max ==countOfTest[1]) return new int[]{1,2};
+        else if(max == countOfTest[0] && max ==countOfTest[2]) return new int[]{1,3};
+        else if(max == countOfTest[1] && max ==countOfTest[2]) return new int[]{2,3};
+        else if(max ==countOfTest[0]) return new int[]{1};
+        else if(max ==countOfTest[1]) return new int[]{2};
+
+        return new int[]{3};
     }
 
     public static void main(String[] args) {
 
-        int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
-        int[] moves = {1,5,3,5,1,2,1,4};
-        int expect = 4;
+        int[] answers = {1,2,3,4,5};
 
-        int answer = new Solution().solution(board,moves) == expect ? 1 : -1 ;
-
-        System.out.println(answer);
+        new Solution().solution(answers);
     }
+
 
 }

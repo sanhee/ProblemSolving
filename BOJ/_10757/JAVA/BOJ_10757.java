85b07331faa64e70b95aa6a1d262f55f42829d6f
noel <68936@naver.com>
2021-01-23 23:25:14 +0900

    [성공] 큰수 합

--- BOJ/_10757/JAVA/BOJ_10757.java
+++ BOJ/_10757/JAVA/BOJ_10757.java
@@ -1,23 +1,28 @@
 package com.example.BOJ._10757.JAVA;
 
 
-// TODO. 알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
+//m 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 public class BOJ_10757 {
+
     public static void main(String[] args) throws IOException {
 
-        BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        char Input = buff.readLine().charAt(0);
+        String[] num = br.readLine().split(" ");
 
-        int ASCII_NUM = (int)Input;
+        BigInteger  a = new BigInteger(num[0]);
+        BigInteger  b = new BigInteger(num[1]);
 
-        System.out.println(ASCII_NUM);
-        
+
+
+        System.out.println(a.add(b));
+
+        br.close();
 
 
     }

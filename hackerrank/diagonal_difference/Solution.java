b3fc07b04768405b977992ae0c353910770b3763
noel <68936@naver.com>
2021-01-27 15:16:40 +0900

    대각선 합 문제 해결

---
+++ hackerrank/diagonal_difference/Solution.java
@@ -0,0 +1,69 @@
+package com.example.hackerrank.diagonal_difference;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+class Result {
+
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        // Write your code here
+
+        int difference = 0;
+
+        int len = arr.get(0).size(); //m 정사각형이므로, 열의 길이만 구함
+
+        int leftToRight = 0;
+        int rightToLight = 0;
+
+        for(int i=0; i<len; i++){
+            leftToRight+=arr.get(i).get(i);
+            rightToLight+=arr.get(i).get((len-1)-i);
+        }
+
+        difference = Math.abs(leftToRight-rightToLight);
+        return difference;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] arrRowTempItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> arrRowItems = new ArrayList<>();
+
+            for (int j = 0; j < n; j++) {
+                int arrItem = Integer.parseInt(arrRowTempItems[j]);
+                arrRowItems.add(arrItem);
+            }
+
+            arr.add(arrRowItems);
+        }
+
+        int result = Result.diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}

de9a00c46af6d3ebe9778ce6d710879d1ee7a514
noel <68936@naver.com>
2021-02-20 03:49:45 +0900

    모범답안 참고 좀더 빨라졌다. 괜히 StringTokeniZER나 변수 낭비를 많이해서 그런것 같다.

--- BOJ/codeplus/basic1/_9093/JAVA/solution.java
+++ BOJ/codeplus/basic1/_9093/JAVA/solution.java
@@ -4,33 +4,31 @@ import java.io.*;
 import java.util.*;
 
 public class solution {
-    //M 처음 내가 작성한 코드
-    //m 메모리: 34840 KB
-    //m 시간: 680 ms
+    //M 모범답안 참고
+    //m 메모리: 29328 KB
+    //m 시간: 596 ms
 
     public static void main(String[] args) {
-
-        Stack<Character> stack = new Stack<>();
 
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
              BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
             int testCaseNum = Integer.parseInt(br.readLine());
 
             for (int testNum = 0; testNum < testCaseNum; testNum++) {
-                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-                while (st.hasMoreTokens()) {
-                    String t = st.nextToken();
-                    int len = t.length();
-                    for (int i = 0; i < len; i++) {
-                        stack.push(t.charAt(i));
+                String separatedLine = br.readLine()+'\n';
+                Stack<Character> stack = new Stack<>();
+
+                for(char word:separatedLine.toCharArray()){
+                    if(word == ' ' || word == '\n'){
+                        while (!stack.empty()){
+                            bw.write(stack.pop());
+                        }
+                        bw.write(word); //m 마지막 'n'을 출력하기 위해 필요함.
+                    }else{
+                        stack.push(word);
                     }
-                    while (!stack.empty()) {
-                        bw.write(stack.pop());
-                    }
-                    bw.write(" ");
                 }
-                bw.newLine();
             }
 
             bw.flush();

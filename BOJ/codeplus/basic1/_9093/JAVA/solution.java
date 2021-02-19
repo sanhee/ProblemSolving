4c0d99d46419f146459e6fed4a892ba6fab83060
noel <68936@naver.com>
2021-02-20 01:40:42 +0900

    초기커밋

---
+++ BOJ/codeplus/basic1/_9093/JAVA/solution.java
@@ -0,0 +1,8 @@
+package com.example.BOJ.codeplus.basic1._9093.JAVA;
+
+public class solution {
+    //M 2021-02-19 PM 10:29
+    public static void main(String[] args) {
+        System.out.println("Hello!!");
+    }
+}

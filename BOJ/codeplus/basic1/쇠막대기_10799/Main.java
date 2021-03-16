fa58ea6a3e79b38411dcb94bdda881e61f4c4188
noel <68936@naver.com>
2021-03-16 19:37:56 +0900

    준비

---
+++ BOJ/codeplus/basic1/쇠막대기_10799/Main.java
@@ -0,0 +1,12 @@
+package com.example.BOJ.codeplus.basic1.쇠막대기_10799;
+
+/**
+ * Created by 68936@naver.com on 2021-03-16 오후 7:37
+ * Blog : https://velog.io/@san
+ * Github : https://github.com/sanhee
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}

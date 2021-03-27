17f8ccd06f67fb7351653456814ba51bf36c8dec
noel <68936@naver.com>
2021-03-27 14:44:20 +0900

    성공

--- BOJ/codeplus/basic1/오등큰수_17299/Main.java
+++ BOJ/codeplus/basic1/오등큰수_17299/Main.java
@@ -1,7 +1,6 @@
 package com.example.BOJ.codeplus.basic1.오등큰수_17299;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
@@ -20,35 +19,39 @@ public class Main {
         final String[] inputLine = br.readLine().split(" ");
         int[] sequence = new int[MAX_COUNT];
         int[] NGF = new int[MAX_COUNT];
-        Map<Integer, Integer> sequenceMap = new HashMap<>();
+        Map<Integer, Integer> frequent = new HashMap<>();
         Stack<Integer> stack = new Stack<>(); // 스택에 쌓으면서 각 요소의 NGF를 구하기 위함임.
 
         for (int i = 0; i < MAX_COUNT; i++) {  //m 수열을 담고, 등장횟수를 계산하는 반복문
             int cursorNumber = Integer.parseInt(inputLine[i]);
             sequence[i] = cursorNumber;
-            if (sequenceMap.containsKey(cursorNumber)) {
-                sequenceMap.put(cursorNumber, sequenceMap.get(cursorNumber) + 1);
+            if (frequent.containsKey(cursorNumber)) {
+                frequent.put(cursorNumber, frequent.get(cursorNumber) + 1);
                 continue;
             }
-            sequenceMap.put(cursorNumber, 1);
+            frequent.put(cursorNumber, 1);
         }
 
         for (int i = 0; i < MAX_COUNT; i++) {
 
             if (stack.isEmpty()) {
-                stack.push(sequence[i]);
+                stack.push(i);
             }
-            while(!stack.isEmpty() && sequenceMap.get(stack.peek()) < sequenceMap.get(sequence[i])){
+            while (!stack.isEmpty() && frequent.get(sequence[stack.peek()]) < frequent.get(sequence[i])) {
                 NGF[stack.pop()] = sequence[i];
             }
-            stack.push(sequence[i]);
+            stack.push(i);
         }
 
-        while (!stack.isEmpty()){
+        while (!stack.isEmpty()) {
             NGF[stack.pop()] = -1;
         }
-        bw.write(Arrays.toString(NGF));
 
+        for(int n:NGF){
+            bw.write(n+" ");
+        }
+
+        bw.flush();
         br.close();
         bw.close();
     }

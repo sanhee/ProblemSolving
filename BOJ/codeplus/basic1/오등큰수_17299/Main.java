f40dd66cbea2e549674f81afba1c1e40560f3d4a
noel <68936@naver.com>
2021-03-27 17:30:55 +0900

    성공2
    - 일반 배열을 이용하니까 더 빨라졌다.

--- BOJ/codeplus/basic1/오등큰수_17299/Main.java
+++ BOJ/codeplus/basic1/오등큰수_17299/Main.java
@@ -1,8 +1,6 @@
 package com.example.BOJ.codeplus.basic1.오등큰수_17299;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Stack;
 
 /**
@@ -19,17 +17,12 @@ public class Main {
         final String[] inputLine = br.readLine().split(" ");
         int[] sequence = new int[MAX_COUNT];
         int[] NGF = new int[MAX_COUNT];
-        Map<Integer, Integer> frequent = new HashMap<>();
+        int[] frequent = new int[1000001]; // 원소 범위: 1 ≤ Ai ≤ 1,000,000
         Stack<Integer> stack = new Stack<>(); // 스택에 쌓으면서 각 요소의 NGF를 구하기 위함임.
 
         for (int i = 0; i < MAX_COUNT; i++) {  //m 수열을 담고, 등장횟수를 계산하는 반복문
-            int cursorNumber = Integer.parseInt(inputLine[i]);
-            sequence[i] = cursorNumber;
-            if (frequent.containsKey(cursorNumber)) {
-                frequent.put(cursorNumber, frequent.get(cursorNumber) + 1);
-                continue;
-            }
-            frequent.put(cursorNumber, 1);
+            sequence[i] = Integer.parseInt(inputLine[i]);
+            frequent[sequence[i]] += 1;
         }
 
         for (int i = 0; i < MAX_COUNT; i++) {
@@ -37,7 +30,7 @@ public class Main {
             if (stack.isEmpty()) {
                 stack.push(i);
             }
-            while (!stack.isEmpty() && frequent.get(sequence[stack.peek()]) < frequent.get(sequence[i])) {
+            while (!stack.isEmpty() && frequent[sequence[stack.peek()]] < frequent[(sequence[i])]) {
                 NGF[stack.pop()] = sequence[i];
             }
             stack.push(i);
@@ -47,8 +40,8 @@ public class Main {
             NGF[stack.pop()] = -1;
         }
 
-        for(int n:NGF){
-            bw.write(n+" ");
+        for (int n : NGF) {
+            bw.write(n + " ");
         }
 
         bw.flush();

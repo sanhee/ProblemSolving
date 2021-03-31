3b8345e654a58ed05e67f7200868c115ab8b482d
noel <68936@naver.com>
2021-03-31 23:50:13 +0900

    준비

---
+++ BOJ/codeplus/basic1/소수찾기_1978/Main.java
@@ -0,0 +1,20 @@
+package com.example.BOJ.codeplus.basic1.소수찾기_1978;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))){
+
+        final int testcaseMax = Integer.parseInt(br.readLine());
+        String[] intputCommand = br.readLine().split(" ");
+
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

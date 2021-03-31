794f40453414c13beab6af4110188313e486f632
noel <68936@naver.com>
2021-04-01 02:37:08 +0900

    성공

--- BOJ/codeplus/basic1/소수찾기_1978/Main.java
+++ BOJ/codeplus/basic1/소수찾기_1978/Main.java
@@ -3,16 +3,30 @@ package com.example.BOJ.codeplus.basic1.소수찾기_1978;
 import java.io.*;
 
 public class Main {
+
+    public static boolean prime(int num){
+        if(num < 2) return false;
+
+        for(int i=2 ; i*i<=num ; i++){
+            if(num%i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
         try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))){
 
         final int testcaseMax = Integer.parseInt(br.readLine());
         String[] intputCommand = br.readLine().split(" ");
+        int check = 0;
+        for(int i =0; i< testcaseMax; i++){
+            check = prime(Integer.parseInt(intputCommand[i])) ? ++check:check;
+        }
 
-
-
-
+        bw.write(String.valueOf(check));
+        bw.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }

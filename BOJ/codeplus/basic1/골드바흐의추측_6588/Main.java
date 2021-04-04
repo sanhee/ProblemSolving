9f671dfc70e2d0e0712e2d0c18320c42afb863ad
noel <68936@naver.com>
2021-04-04 20:29:40 +0900

    준비:

---
+++ BOJ/codeplus/basic1/골드바흐의추측_6588/Main.java
@@ -0,0 +1,14 @@
+package com.example.BOJ.codeplus.basic1.골드바흐의추측_6588;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+
+
+            bw.flush();
+        }
+    }
+}

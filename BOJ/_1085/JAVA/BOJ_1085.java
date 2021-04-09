30bfe1fa7428f0b51b867ace1548512f5338138c
noel <68936@naver.com>
2021-04-09 23:46:16 +0900

    간만에 복습해봤는데 왜이리 헤맸는지 참..

--- BOJ/_1085/JAVA/BOJ_1085.java
+++ BOJ/_1085/JAVA/BOJ_1085.java
@@ -1,24 +1,27 @@
 package com.example.BOJ._1085.JAVA;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
 
 public class BOJ_1085 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer InputStr = new StringTokenizer(buff.readLine()," ");
+        String[] inputCommand = br.readLine().split(" ");
 
-        int x = Integer.parseInt(InputStr.nextToken());
-        int y = Integer.parseInt(InputStr.nextToken());
-        int w = Integer.parseInt(InputStr.nextToken());
-        int h = Integer.parseInt(InputStr.nextToken());
+        int x = Integer.parseInt(inputCommand[0]); // x->0 까지 거리
+        int y = Integer.parseInt(inputCommand[1]);  // y->0 까지 거리
+        int w = Integer.parseInt(inputCommand[2]);
+        int h = Integer.parseInt(inputCommand[3]);
 
-        int x_min = Math.min(x,w-x);
-        int y_min = Math.min(y,h-y);
+        x = Math.min(x, w - x); // x-> w까지 거리와 어떤게 더 가까운지 비교
+        y = Math.min(y, h - y);
 
-        System.out.println(Math.min(x_min,y_min));
+        bw.write(String.valueOf(Math.min(x,y)));
+        bw.flush();
+
+        bw.close();
+        br.close();
+
     }
 }

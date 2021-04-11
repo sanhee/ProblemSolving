667491b02e8fb37bd7ee6c47b18d9857e3bd98ef
noel <68936@naver.com>
2021-04-11 23:39:23 +0900

    준비

---
+++ BOJ/단어정렬_1181/Main.java
@@ -0,0 +1,16 @@
+package com.example.BOJ.단어정렬_1181;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.flush();
+
+        bw.close();
+        br.close();
+
+    }
+}

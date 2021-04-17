113c66baf0cc11202df03f04c4848808d0457d4d
noel <68936@naver.com>
2021-04-17 22:23:03 +0900

    성공

---
+++ programmers/나누어떨어지는숫자배열_12910/Solution.java
@@ -0,0 +1,31 @@
+package com.example.programmers.나누어떨어지는숫자배열_12910;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+    public int[] solution(int[] arr, int divisor) {
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int n : arr) {
+            if (n % divisor == 0) {
+                list.add(n);
+            }
+        }
+
+        int[] answer = new int[list.size()];
+
+        if(answer.length == 0){
+            return new int[]{-1};
+        }
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+        Arrays.sort(answer);
+
+        return answer;
+    }
+}

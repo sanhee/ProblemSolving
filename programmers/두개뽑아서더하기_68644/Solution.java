290e714d0db99a96de25e7fa7fa16d45fa945657
noel <68936@naver.com>
2021-04-18 23:57:43 +0900

    성공

---
+++ programmers/두개뽑아서더하기_68644/Solution.java
@@ -0,0 +1,28 @@
+package com.example.programmers.두개뽑아서더하기_68644;
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            for(int j=i+1; j<numbers.length; j++){
+                set.add(numbers[i]+numbers[j]);
+            }
+        }
+        int[] answer = new int[set.size()];
+
+        Iterator<Integer> iterator = set.iterator();
+
+        int cnt = 0;
+        while(iterator.hasNext()){
+            answer[cnt] = iterator.next();
+            cnt++;
+        }
+
+        Arrays.sort(answer);
+        return answer;
+    }
+}

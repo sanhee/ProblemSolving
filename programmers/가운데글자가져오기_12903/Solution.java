a98395fa024f088bcf187a711c733c03ac0ab316
noel <68936@naver.com>
2021-04-18 17:27:07 +0900

    성공

---
+++ programmers/가운데글자가져오기_12903/Solution.java
@@ -0,0 +1,17 @@
+package com.example.programmers.가운데글자가져오기_12903;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution("qwer"));
+    }
+    public String solution(String s) {
+
+        if(s.length()%2 != 0){
+            return s.charAt(s.length()/2)+"";
+        }else{
+            return s.charAt(s.length()/2 -1)+""+s.charAt(s.length()/2);
+        }
+
+    }
+}

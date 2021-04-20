037766e1c3ebde17e18339e1687549879b025615
sanhee <68936@naver.com>
2021-04-21 01:22:08 +0900

    성공

---
+++ programmers/완주하지못한선수_42576/Solution.java
@@ -0,0 +1,20 @@
+package com.example.programmers.완주하지못한선수_42576;
+
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for (int i = 0; i < completion.length; i++) {
+            if(!participant[i].equals(completion[i])){
+                return participant[i];
+            }
+        }
+
+        return participant[completion.length];
+
+    }
+}

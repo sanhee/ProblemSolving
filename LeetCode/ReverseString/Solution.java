e62cfe4fdd44a70f5ea5c1f2b9a7a254a7e81e7c
sanhee <68936@naver.com>
2021-04-24 22:50:03 +0900

    성공
    파이썬으로 풀면 너무 신기하다.
    
    temp가 없이 바로 스위칭을 할 수 있네?

---
+++ LeetCode/ReverseString/Solution.java
@@ -0,0 +1,24 @@
+package com.example.LeetCode.ReverseString;
+
+public  class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length-1;
+        char temp;
+
+        while(left < right){
+            temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args){
+        char[] s = new char[]{'a','b','c'};
+        new Solution().reverseString(s);
+        System.out.println(s);
+    }
+}

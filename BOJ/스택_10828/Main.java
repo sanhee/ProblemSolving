ed7a12615f76fa40eaff1ca68e8d3a1b69585550
sanhee <68936@naver.com>
2021-04-26 13:59:46 +0900

    성공

---
+++ BOJ/스택_10828/Main.java
@@ -0,0 +1,60 @@
+package com.example.BOJ.스택_10828;
+
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Stack<Integer> stack = new Stack<>();
+
+        int testNum = Integer.parseInt(br.readLine());
+
+        while(testNum-- > 0){
+
+            String[] command = br.readLine().split(" ");
+
+            switch (command[0]){
+
+                case "push":
+                    stack.push(Integer.parseInt(command[1]));
+                    break;
+                case "pop":
+                    if(stack.isEmpty()){
+                        bw.write("-1");
+                    }else {
+                        bw.write(stack.pop()+"");
+                    }
+                    bw.newLine();
+                    break;
+                case "size":
+                    bw.write(stack.size()+"");
+                    bw.newLine();
+                    break;
+                case "empty":
+                    if(stack.isEmpty()){
+                        bw.write("1");
+                    }else{
+                        bw.write("0");
+                    }
+                    bw.newLine();
+                break;
+                case "top":
+                    if(stack.isEmpty()){
+                        bw.write("-1");
+                    }else{
+                        bw.write(stack.peek()+"");
+                    }
+                    bw.newLine();
+                    break;
+            }
+        }
+
+        bw.flush();
+
+    }
+}

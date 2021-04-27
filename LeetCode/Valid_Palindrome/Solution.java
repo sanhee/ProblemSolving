a60f97fab2bb1abf8e6aef7d062c7b4e2e1b17bb
sanhee <68936@naver.com>
2021-04-27 17:39:50 +0900

    정규식 몰라서 풀이봄

---
+++ LeetCode/Valid_Palindrome/Solution.java
@@ -0,0 +1,24 @@
+package com.example.LeetCode.Valid_Palindrome;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        new Solution().isPalindrome("A man, a plan, a canal: Panama");
+    }
+
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]","").toUpperCase();
+
+        int forward = 0;
+        int backward = s.length()-1;
+
+        while(forward<backward){
+            if(s.charAt(forward) != s.charAt((backward))){
+                return false;
+            }
+            forward++;
+            backward--;
+        }
+        return true;
+    }
+}

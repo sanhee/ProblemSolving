3865da8fb988a9b706fe4a4dbbd47f3cfee1e75d
sanhee <68936@naver.com>
2021-04-29 23:56:00 +0900

    후위표기식 : ?

--- BOJ/후위표기식2_1935/Main.java
+++ BOJ/후위표기식2_1935/Main.java
@@ -1,52 +1,28 @@
 package com.example.BOJ.후위표기식2_1935;
 
 import java.io.*;
-import java.util.Stack;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        String postfix = br.readLine();
-        char[] arrayPostfix = postfix.toCharArray();
+        int numberOfOperand = Integer.parseInt(br.readLine());
+        char[] expression = br.readLine().toCharArray();
 
+        for(char word : expression){
+            if(Character.isAlphabetic(word)){
 
-        Stack<Double> stack = new Stack<>();
-
-        for(int i=0; i< arrayPostfix.length;i++) {
-            if(Character.isAlphabetic(arrayPostfix[i])){
-                stack.push(Double.parseDouble(br.readLine()));
-            }else if(stack.size()>1){
-                char operator = arrayPostfix[i];
-                double operand2 = stack.pop();
-                double operand1 = stack.pop();
-
-                stack.push(calculator(operand1,operator,operand2));
             }
         }
 
-        while (!stack.isEmpty()){
-            bw.write(String.format("%.2f",stack.pop()));  // 소수 두번째 자리까지
+        Map<Character, Integer> mapOfOperand = new HashMap<>();
+        for (int i = 0; i < numberOfOperand; i++) {
+
         }
 
         bw.flush();
-    }
-
-    public static Double calculator(double operand1, char operator, double operand2){
-
-        switch (operator){
-            case '+':
-                return operand1+operand2;
-            case '-':
-                return operand1-operand2;
-            case '*':
-                return operand1*operand2;
-            case '/':
-                return operand1/operand2;
-        }
-        return 0.0;
     }
 }

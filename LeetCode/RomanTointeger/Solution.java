5f37c68d39a5ccb34fd2836444f1534c4f53e5d0
sanhee <68936@naver.com>
2021-05-01 15:13:27 +0900

    로마정수 - 성공

--- LeetCode/RomanTointeger/Solution.java
+++ LeetCode/RomanTointeger/Solution.java
@@ -1,83 +1,41 @@
 package com.example.LeetCode.RomanTointeger;
 
+// * 다음 요소의 값이 현재 요소보다 크면 마이너스가 핵심인 문제
 public class Solution {
 
-    enum roman {
-        I(1),
-        V(5),
-        X(10),
-        L(50),
-        C(100),
-        D(500),
-        M(1000);
+    enum RomanNumber {
+        I(1), V(5), X(10), L(50), C(100), D(500), M(1000);
+        public int value;
 
-        private int value;
-
-        roman(int i) {
+        RomanNumber(int i) {
             value = i;
         }
-
-        public int getValue() {
-            return value;
-        }
-    }
-
-    public static void main(String[] args) {
-        int result = new Solution().romanToInt("MCMXCIV");
-        int expected = 1994;
-
-        System.out.println((result == expected) + ":" + result);
     }
 
     public int romanToInt(String s) {
-        String[] array = s.split("");
 
+        String[] inputArray = s.split("");
+
+        int prev = 0;
+        int current = 0;
         int result = 0;
 
-        for (int i = 0; i < array.length; i++) {
-            if (array[i].equals("I")) {
-                if (array.length > i + 1) {
-                    if (array[i + 1].equals("V") || array[i + 1].equals("X")) {
-                        result = roman.valueOf(array[i + 1]).getValue() - roman.valueOf(array[i]).getValue();
-                        i= i+1;
-                        continue;
-                    } else {
-                        result += roman.valueOf(array[i]).getValue();
-                    }
-                } else {
-                    result += roman.valueOf(array[i]).getValue();
-                }
-            } else if (array[i].equals("X")) {
-                if (array.length > i + 1){
-                    if (array[i + 1].equals("L") || array[i + 1].equals("C")) {
-                        result = roman.valueOf(array[i + 1]).getValue() - roman.valueOf(array[i]).getValue();
-                        i= i+1;
-                        continue;
-                    } else {
-                        result += roman.valueOf(array[i]).getValue();
-                    }
+        for (int i = 0; i < inputArray.length; i++) {
+            if(i>0) {
+                current = RomanNumber.valueOf(inputArray[i]).value;
+                if(prev >= current){
+                    result += current;
                 }else{
-                    result += roman.valueOf(array[i]).getValue();
+                    result += (current-(prev*2));  // 이전 IF문 으로 이미 이전의 작은 값이 더해져있으므로, IV 같은 연산을 하기 위해서는 이전 요소를 두번 빼줘야 하낟.
                 }
-            } else if (array[i].equals("C")) {
-                if(array.length > i + 1) {
-                    if (array[i + 1].equals("D") || array[i + 1].equals("M")) {
-                        result = roman.valueOf(array[i + 1]).getValue() - roman.valueOf(array[i]).getValue();
-                        i= i+1;
-                        continue;
-                    } else {
-                        result += roman.valueOf(array[i]).getValue();
-                    }
-                }else{
-                    result += roman.valueOf(array[i]).getValue();
-                }
-            } else{
-                result += roman.valueOf(array[i]).getValue();
+                prev = current;
+                continue;
             }
-
+           prev = RomanNumber.valueOf(inputArray[i]).value;
+           result += prev;
         }
-
         return result;
-
     }
+
 }
+

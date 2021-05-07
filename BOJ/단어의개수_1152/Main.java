310ff5f84a3326d9a513a23b59083dfc9694d73b
sanhee <68936@naver.com>
2021-05-07 23:40:26 +0900

    백준: 1152. 브론즈2 - 단어의 개수 (성공)
    오늘은 큰 생각이 없어서 완전 쉬운 문제 하나 풀었다.

---
+++ BOJ/단어의개수_1152/Main.java
@@ -0,0 +1,17 @@
+package com.example.BOJ.단어의개수_1152;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        String[] array = str.split(" ");
+
+
+        System.out.println(array.length);
+
+    }
+}

4bc629889093783b11d57d6fd6615cef02c91559
sanhee <68936@naver.com>
2021-05-09 23:51:12 +0900

    [준비] 백준: 1157. 브론즈1 - 단어공부

---
+++ BOJ/단어공부_1157/Main.java
@@ -0,0 +1,14 @@
+package com.example.BOJ.단어공부_1157;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+        bw.flush();
+    }
+}

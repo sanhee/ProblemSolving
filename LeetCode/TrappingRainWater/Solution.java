d5445540e676c28efb55bf6fa98e27cafaea2b17
sanhee <68936@naver.com>
2021-05-13 00:21:25 +0900

    [성공:투포인터] Leetcode: 빗물 트래핑
    - 왼쪽과 오른쪽 끝에 포인터를 두고, 좌우 어느 쪽이든 낮은 쪽은 높은 쪽을 향해서 포인터가 가운데로 점점 이동한다.
    
     - 최대 높이의 막대는 전체 부피에 영향을 끼치지 않는다.
    왼쪽, 오른쪽을 가르는 장벽 역할

--- LeetCode/TrappingRainWater/Solution.java
+++ LeetCode/TrappingRainWater/Solution.java
@@ -3,27 +3,37 @@ package com.example.LeetCode.TrappingRainWater;
 public class Solution {
     public int trap(int[] height) {
 
-        if (height.length == 0) return 0;
-
         int left = 0;
         int right = height.length - 1;
-        int[] left_max = new int[height.length];
-        int[] right_max = new int[height.length];
+        int left_max = 0;
+        int right_max = 0;
         int volum = 0;
 
 
-        left_max[0] = height[0];
-        for(int i=1; i<height.length; i++){
-            left_max[i] = Math.max(height[i], left_max[i-1]);
-        }
+        while (left < right) {
 
-        right_max[height.length-1] = height[height.length-1];
-        for(int i= height.length-2; i>=0; i--){
-            right_max[i] = Math.max(height[i], right_max[i+1]);
-        }
+            if (height[left] <= height[right]) {
 
-        for(int i=1 ; i<height.length-1 ; i++){
-            volum += Math.min(left_max[i], right_max[i]) - height[i];
+                if (height[left] >= left_max) {
+                    left_max = height[left];
+                } else {
+                    volum += left_max - height[left];
+                }
+
+
+                left++;
+
+            } else {
+
+                if (height[right] >= right_max) {
+                    right_max = height[right];
+                } else {
+                    volum += right_max - height[right];
+                }
+
+                right--;
+            }
+
         }
 
         return volum;

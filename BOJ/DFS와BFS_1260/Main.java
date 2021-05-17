64ebc026d909fe4484e0b5d93e28eaedeecd3c9a
sanhee <68936@naver.com>
2021-05-18 02:21:26 +0900

    [#성공] 백준1260: DFS와 BFS
    - 안보고따라해봄

--- BOJ/DFS와BFS_1260/Main.java
+++ BOJ/DFS와BFS_1260/Main.java
@@ -4,77 +4,75 @@ import java.util.*;
 
 public class Main {
 
-    private static int N;
-    private static int M;
-    private static int V;
+    private static int N; // 정점의 개수
+    private static int M; // 간선의 개수
+    private static int V; // 탐색을 시작할 정점의 번호
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Map<Integer, Set<Integer>> graph = new HashMap<>(); // *
 
-        N = scanner.nextInt(); // 정점 개수
-        M = scanner.nextInt(); // 간선 개수
-        V = scanner.nextInt(); // 시작할 정점의 번호
+        N = scanner.nextInt();
+        M = scanner.nextInt();
+        V = scanner.nextInt();
 
-        Map<Integer, Set<Integer>> graph = new HashMap<>(); // graph 1->2  1->3  1->4 1->%
-        initGraph(graph, scanner);
-
-        boolean[] visited = new boolean[N + 1];
+        createGraph(graph, scanner); // *
+        boolean[] visited = new boolean[N + 1];  // N번째 정점의 상태도 변경해야 하므로 +1
         dfs(graph, V, visited);
         System.out.println();
-        Arrays.fill(visited, false);
-        bfs(graph, V, visited);
 
+        Arrays.fill(visited, false); // dfs 연산에서 사용된 값들을 초기화
+        bfs(graph, V, visited);
     }
 
-    private static void initGraph(Map<Integer, Set<Integer>> graph, Scanner scanner) {
-        for(int i=0; i<M; i++){
-            int v1 = scanner.nextInt(); //정점 A
-            int v2 = scanner.nextInt(); // 정점 B
+    public static void createGraph(Map<Integer, Set<Integer>> graph, Scanner scanner) {
+        for (int i = 0; i < M; i++) {
+            int v1 = scanner.nextInt();
+            int v2 = scanner.nextInt();
 
-            Set<Integer> v1Edges = graph.getOrDefault(v1, new TreeSet<>()); // 왜 Treeset일까..?
-            v1Edges.add(v2);
-            graph.put(v1, v1Edges);
+            Set<Integer> v1Edge = graph.getOrDefault(v1, new TreeSet<>());
+            v1Edge.add(v2);
 
-            Set<Integer> v2Edges = graph.getOrDefault(v2, new HashSet<>());
-            v2Edges.add(v1);
-            graph.put(v2, v2Edges);
+            graph.put(v1, v1Edge);
+
+            // 양방향 연결이 돼있다고 했으므로,
+            Set<Integer> v2Edge = graph.getOrDefault(v2, new TreeSet<>());
+            v2Edge.add(v1);
+
+            graph.put(v2, v2Edge);
         }
     }
 
-    private static void dfs(Map<Integer, Set<Integer>> graph, Integer vertex, boolean[] visited){
-        if(visited[vertex]){ // 방문한 정점인 경우
+    public static void dfs(Map<Integer, Set<Integer>> graph, int vertex, boolean[] visited) {
+        if (visited[vertex]) { // 이미 방문한 정점이라면
             return;
         }
-
-        System.out.print(vertex + " "); // 방문한 정점을 출력한다.
+        System.out.print(vertex + " ");
         visited[vertex] = true;
 
-        for (Integer v : graph.getOrDefault(vertex, new TreeSet<>())){   // 정점과 인접한 정점을 탐색한다.
-            dfs(graph,v,visited);
+        for (Integer v : graph.getOrDefault(vertex, new TreeSet<>())) {
+            dfs(graph, v, visited);
         }
     }
 
-    private static void bfs(Map<Integer, Set<Integer>> graph, Integer vertex, boolean[] visited){
+    public static void bfs(Map<Integer, Set<Integer>> graph, int vertex, boolean[] visited) {
         Queue<Integer> queue = new LinkedList<>();
-
         queue.add(vertex);
         visited[vertex] = true;
 
-        while(!queue.isEmpty()){
-            Integer v0 = queue.poll();
-            System.out.print(v0 + " ");
+        while (!queue.isEmpty()) {
+            int v = queue.poll();
+            System.out.print(v + " ");
 
-            for(Integer v : graph.getOrDefault(v0, new TreeSet<>())){
-                if(!visited[v]){
-                    queue.add(v);
-                    visited[v] = true;
+            for (Integer adjacentV : graph.getOrDefault(v, new TreeSet<>())) {
+                if (!visited[adjacentV]) { // 방문하지 않은 것만 추가
+                    queue.add(adjacentV);
+                    visited[adjacentV] = true;
                 }
             }
 
         }
 
-
     }
-
 
 }

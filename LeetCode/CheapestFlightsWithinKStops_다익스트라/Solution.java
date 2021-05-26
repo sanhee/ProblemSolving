c8c663061716e7f644bc02f11f748296aec383a3
sanhee <68936@naver.com>
2021-05-26 19:04:14 +0900

    [실패] - K 경유지 내 가장 저렴한 항공권 : 가중치 조건문 추가

--- LeetCode/CheapestFlightsWithinKStops_다익스트라/Solution.java
+++ LeetCode/CheapestFlightsWithinKStops_다익스트라/Solution.java
@@ -1,9 +1,6 @@
 package com.example.LeetCode.CheapestFlightsWithinKStops_다익스트라;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Solution {
 
@@ -13,7 +10,6 @@ public class Solution {
         if (flights.length == 0) {
             return -1;
         }
-
 
         HashMap<Integer, List<int[]>> graph = new HashMap<>();
 
@@ -31,11 +27,14 @@ public class Solution {
         }
 
 
+        Map<Integer, Integer> weight = new HashMap<>();
+
+
         // 인접 노드로 갈 때 최소비용인 곳으로 가야하므로, 정렬이 가능한 PriorityQueue 사용
         // 비용 기준 오름차순 정렬
         PriorityQueue<Node> q = new PriorityQueue<Node>((a, b) -> (a.cost - b.cost));
 
-        // 인접노드???
+        // [목적지, 비용, 경유횟수] 초깃값 정하기, 자기자신은 0비용이고, 경유를 -1부터 시작해야 한번이라도 움직일 수 있음.
         q.add(new Node(src, 0, -1));
 
 
@@ -43,16 +42,26 @@ public class Solution {
 
             Node current = q.poll();
 
+            // 큐에서 꺼낸 현재 도시가 목적지와 같다면, 현재 비용을 리턴하고 끝낸다.
             if (current.city == dst) {
                 return current.cost;
             }
 
-            // 경유할 수 있는 횟수가 남았다면, 다른 인접 노드 탐색
+            // 경유할 수 있는 횟수가 남았다면, 현재 노드의 인접 노드 그래프 탐색 (BFS)
             if (current.stop < K) {
+                // 키 값으로 그래프에서 나온 value 이므로, 인근 노드의 {목적지 노드, 비용} 리스트가 나올거임.
                 List<int[]> nexts = graph.getOrDefault(current.city, new ArrayList<int[]>());
 
+
                 for (int[] next : nexts) {
-                    q.add(new Node(next[0], current.cost + next[1], current.stop + 1));
+
+                    int to = next[0];
+                    int alt = current.cost + next[1];
+
+                    if (weight.getOrDefault(to, 99999) > alt) {
+                        weight.put(to, alt);
+                        q.add(new Node(to, alt, current.stop + 1));
+                    }
                 }
             }
 
@@ -60,8 +69,24 @@ public class Solution {
         return -1;
     }
 
-}
+    public static void main(String[] args) {
+        int n = 3;
+        int[][] flights = {{0, 1, 100},
+                {1, 2, 100},
+                {0, 2, 500}
+        };
+        int src = 0;
+        int dst = 2;
+        int k = 0;
 
+        if (new Solution().findCheapestPrice(n, flights, src, dst, k) == 500) {
+            System.out.println("CASE1. 성공");
+        } else {
+            System.out.println("CASE1. 실패");
+        }
+
+    }
+}
 
 class Node {
     int city;

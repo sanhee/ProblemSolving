27e98b770a81abb101b8329aa1991aad3617479e
sanhee <68936@naver.com>
2021-06-16 13:37:24 +0900

    [성공] 물병 문제 - BitCount 활용해서 품
    N	K	NEW
    1	1	0	1
    2	1	0	10
    3	1	1	11
    4	1	0	100
    5	1	3	101
    6	1	2	110
    7	1	1	111
    8	1	0	1000
    9	1	7	1001
    10	1	6	1010
    11	1	5	1011
    12	1	4	1100
    13	1	3	1101
    14	1	2	1110
    15	1	1	1111
    16	1	0	10000

--- BOJ/_1052/JAVA/BOJ_1052.java
+++ BOJ/_1052/JAVA/BOJ_1052.java
@@ -4,47 +4,26 @@ package com.example.BOJ._1052.JAVA;
 import java.io.*;
 
 public class BOJ_1052 {
-
-    // Set up : I/O
-    static StringBuilder output = new StringBuilder();
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
 
-        // Set up : Global Variables
-        /* None */
+        String[] inputs = br.readLine().split(" ");
 
-            // Set up : Input
-            String[] inputs = br.readLine().split(" ");
-            int N = Integer.parseInt(inputs[0]);
-            int K = Integer.parseInt(inputs[1]);
+        int N = Integer.parseInt(inputs[0]);
+        int K = Integer.parseInt(inputs[1]);
 
-            // Process
-            int ans = 0;
-            int bc = Integer.bitCount(N);
-            int ofs = 1;
-            while (bc > K) {
-                while ((N&ofs) == 0) {
-                    ofs <<= 1;
-                }
-                ans += ofs;
-                N += ofs;
-                bc = Integer.bitCount(N);
-            }
+        int bitCount = Integer.bitCount(N);
 
-            // Control : Output
-            output.append(ans);
-
-            bw.write(output.toString());
-            bw.flush();
-
-            // Close up : I/O
-            br.close();
-            bw.close();
+        int temp = N;
+        while (bitCount > K) {
+            temp++;
+            bitCount = Integer.bitCount(temp);
         }
 
-        // Helper Functions
-        /* None */
+        bw.write(temp - N + "");
+        bw.flush();
+    }
 
 }

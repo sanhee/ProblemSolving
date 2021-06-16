b6fd35b357da86a6ca200e63e3f58f88dcf0ec6c
sanhee <68936@naver.com>
2021-06-16 13:43:09 +0900

    [성공/주석추가] 물병 문제 - BitCount 활용해서 품
    N	K	NEW
    1	1	0	1
    2	1	0	10
    3	1	1	11
    4	1	0	100
    5	1	3	101
    6	1	2	110
    7	1	1	111
    8	1	0	1000
    9	1	7	1001
    10	1	6	1010
    11	1	5	1011
    12	1	4	1100
    13	1	3	1101
    14	1	2	1110
    15	1	1	1111
    16	1	0	10000

--- BOJ/_1052/JAVA/BOJ_1052.java
+++ BOJ/_1052/JAVA/BOJ_1052.java
@@ -11,11 +11,16 @@ public class BOJ_1052 {
 
         String[] inputs = br.readLine().split(" ");
 
-        int N = Integer.parseInt(inputs[0]);
-        int K = Integer.parseInt(inputs[1]);
+        int N = Integer.parseInt(inputs[0]); // 현재 물병의 개수
+        int K = Integer.parseInt(inputs[1]); // 한번에 옮길 수 있는 물병의 개수
 
+        // 물병의 개수의 비트 개수를 구하면, 한번에 옮길 수 있는지 판단할 수 있음.
+        // ex) N:3 (11), K:1
         int bitCount = Integer.bitCount(N);
 
+        // 2의 거듭제곱인 경우에 물병을 한번에 옮길 수 있음.
+        // 2의 거듭제곱의 bit는 1이 한개만 존재함. 1 2 4 8 16..
+        // 필요한 물병은 현재 물병 이후, 2의 거듭제곱인 물병의 개수에서 빼면 구할 수 있음.
         int temp = N;
         while (bitCount > K) {
             temp++;

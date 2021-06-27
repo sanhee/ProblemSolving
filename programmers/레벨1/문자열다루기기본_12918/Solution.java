ed875caae0c183b477bb4a489be2e2e7ff7f5f13
sanhee <68936@naver.com>
2021-06-27 14:53:16 +0900

    성공: 문자열 다루기 - 정규표현식

--- programmers/레벨1/문자열다루기기본_12918/Solution.java
+++ programmers/레벨1/문자열다루기기본_12918/Solution.java
@@ -2,18 +2,7 @@ package com.example.programmers.레벨1.문자열다루기기본_12918;
 
 class Solution {
     public boolean solution(String s) {
-        if(s.length() != 4 && s.length() != 6) return false;
-
-        boolean isCharacter = false;
-        for(char word : s.toCharArray()){
-            if(isCharacter){
-                return false;
-            }
-            if(!Character.isDigit(word)){
-                isCharacter = true;
-            }
-
-        }
-        return true;
+        if (s.length() == 4 || s.length() == 6) return s.matches("(^[0-9]*$)");
+        return false;
     }
 }

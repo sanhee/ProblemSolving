7b1e5b20aaa9758508d0517367c6dea20cf9a87f
sanhee <68936@naver.com>
2021-07-13 02:07:29 +0900

    성공: 서울에서 김서방 찾기 (단순 반복문으로 개선)
    #1
    
    기존 리스트를 이용한 방식은 공간을 낭비하다는 의견이 많아,
    다소 코드의 길이가 증가하더라도, 기본 로직을 이용하기로 했다.

--- programmers/레벨1/서울에서김서방찾기/Solution.java
+++ programmers/레벨1/서울에서김서방찾기/Solution.java
@@ -1,12 +1,15 @@
 package com.example.programmers.레벨1.서울에서김서방찾기;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 class Solution {
     public String solution(String[] seoul) {
-        List<String> seoulList = new ArrayList<>(Arrays.asList(seoul));
-        return "김서방은 "+seoulList.indexOf("Kim")+"에 있다";
+        int index = -1;
+
+        for(int i=0; i<seoul.length; i++){
+            if(seoul[i].equals("Kim")){
+                index = i;
+                break;
+            }
+        }
+        return "김서방은 "+index+"에 있다";
     }
 }

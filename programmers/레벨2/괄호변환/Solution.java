c3d8f31076df45652d966d7068fee5efbb7d2ccb
sanhee <68936@naver.com>
2021-07-23 01:09:28 +0900

    [성공/2회차] 괄호변환
    #3 #4
    
    substring의 exclude함을 잊지말자

--- programmers/레벨2/괄호변환/Solution.java
+++ programmers/레벨2/괄호변환/Solution.java
@@ -17,32 +17,32 @@ public class Solution {
                 count--;
             }
             if (count == 0) { // 가장 짧은 균형잡힌 괄호의 조건
-                return index;
+                return index+1; // 오 마이갓 ...+1을 해줘야 하는 이유 substring의 마지막은 exclusive하기 때문임.
             }
             index++;
         }
         return strs.length;
     }
 
-    private boolean isBalanced(StringBuilder u){
+    private boolean isBalanced(StringBuilder u) {
         Stack<Character> stack = new Stack<>();
         char[] str = u.toString().toCharArray();
 
-        for(char input : str){
-            if(stack.empty()){
+        for (char input : str) {
+            if (stack.empty()) {
                 stack.push(input);
-            }else {
-                if(input == ')' && stack.peek() == '('){
+            } else {
+                if (input == ')' && stack.peek() == '(') {
                     stack.pop();
-                }else{
+                } else {
                     stack.push(input);
                 }
             }
         }
 
-        if(stack.empty()){
+        if (stack.empty()) {
             return true;
-        }else{
+        } else {
             return false;
         }
 
@@ -53,29 +53,34 @@ public class Solution {
 
         int index = split(p);
 
-        StringBuilder u = new StringBuilder(p.substring(0,index));
+        StringBuilder u = new StringBuilder(p.substring(0, index));
         StringBuilder v = new StringBuilder(p.substring(index));
 
-        if(isBalanced(u)){
+        if (isBalanced(u)) {
             u.append(solution(v.toString()));
             return u.toString();
-        }else{
-            StringBuilder sb = new StringBuilder();
-            sb.append('(');
-            sb.append(solution(v.toString()));
-            sb.append(')');
-
-            for(int i= 1; i< u.length()-1 ; i++){
-                if(u.charAt(i) == '(') {
-                    u.replace(i, i, ")");
-                }else{
-                    u.replace(i, i, "(");
-                }
-            }
-            sb.append(u);
-
-            return sb.toString();
         }
 
+        StringBuilder sb = new StringBuilder();
+        sb.append('(');
+        sb.append(solution(v.toString()));
+        sb.append(')');
+
+        for (int i = 1; i < u.length() - 1; i++) {
+            if (u.charAt(i) == '(') {
+                sb.append(")");
+            } else {
+                sb.append("(");
+            }
+        }
+
+        return sb.toString();
     }
+
+    public static void main(String[] args) {
+        if(new Solution().solution("(()())()").equals("(()())()")){
+            System.out.println("통과");
+        }
+    }
+
 }

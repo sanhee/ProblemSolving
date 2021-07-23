d18d8f119dafbd17aebb185fdd87553ac71c8f56
sanhee <68936@naver.com>
2021-07-24 02:23:15 +0900

    [성공/2회차] 레벨1. 시저암호 - 아스키코드/대소문자
    #1 #4
    
    오답분석.
    
    - 문자를 n에 따라 이동시킬 때, 문자가 끝과 끝에서 순환되는 로직만 처리해주면 된다고 생각했다.
    - 따라서, z와 Z에 관해서만 문자가 순환하도록(c - 25 + n - 1)를 처리해줬는데, 잘 생각해보니 다른 문자에서도 이동할 때 순환되는 로직을 만들어야했다.
    - 너무 경험이 부족하다.

--- programmers/레벨1/시저암호/Solution.java
+++ programmers/레벨1/시저암호/Solution.java
@@ -11,16 +11,27 @@ class Solution {
 
 
         for(char c : str){
+
             if(c == ' '){
                 sb.append(c);
             }
-            else if ( (int) c == 90 || (int) c == 122){
-                sb.append((char)(c-25+n-1));
-            }else{
-                sb.append((char)(c+n));
-            }
-        }
 
+            else if( 'A' <= (int) c  && (int)c <= 'Z'){ // 대문자
+                if(c+n > 'Z') {
+                    sb.append((char) (c - 25 + n - 1));  // n-1인 이유는 25를 빼는 과정도 생각해야 하기 때문임.
+                }else{
+                    sb.append((char)(c+n));
+                }
+
+            }else if( 'a' <= (int) c && (int)c <= 'z') { // 소문자
+                if(c+n > 'z') {
+                    sb.append((char) (c - 25 + n - 1));
+                }else{
+                    sb.append((char)(c+n));
+                }
+            }
+
+        }
 
         return sb.toString();
     }

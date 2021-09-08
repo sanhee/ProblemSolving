8e88d1dd87c33d8a55a6152454eba2c2232d329d
sanhee <68936@naver.com>
2021-09-08 11:36:02 +0900

    [완전탐색/복습] - N-Queen
    #15

--- BOJ/완전탐색/N_Queen/Main.java
+++ BOJ/완전탐색/N_Queen/Main.java
@@ -1,73 +1,61 @@
 package com.example.BOJ.완전탐색.N_Queen;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int N, ans;
-    private static int[] col; // col[i] : i번의 행의 퀸은 col[i]번 열에 놓았다는 기록
-    // row 번 ~ N 번 행에 대해서 가능한 퀸을 놓는 경우의 수 구하기
-    private static StringBuilder sb = new StringBuilder();
 
-    public static void input(){
-        FastReader scan = new FastReader();
+    private static int[] column;
+    private static int result;
+    private static FastReader scan = new FastReader();
+    private static int N;
+
+    private static void input(){
         N = scan.nextInt();
-        col = new int[N+1];
+        column = new int[N];
     }
 
-    public static void rec_func(int row){
-        if(row == N+1){ // 각 행마다 하나씩 잘 놓았다.
-            ans++;
+
+    private static void rec_func(int row) {
+
+        if(row == N){
+            result++;
         }else{
-            for(int c = 1; c<= N; c++){
-                boolean possible = true;
-                // valid check (row, c)
-                // 이전에 놓였던 퀸을 봄
-                for(int i=1; i<=row-1; i++){
-                    if(attackable(row, c, i, col[i])){
-                        possible = false;
-                        break;
-                    }
-                }
-                if(possible) {
-                    col[row] = c;
-                    rec_func(row + 1);
-                    col[row] = 0;
-                }
-            }
-        }
-    }
 
-    private static boolean validity_check() {
+           for(int i=0; i<N; i++){
+               boolean possible = true;
 
-        for(int i=1; i<=N; i++){
-            // (i, col[i])
-            for(int j=1; j<i; j++){
-                // (j, col[j])
-                if(attackable(i, col[i], j, col[j])){
-                    return false;
-                }
-                return true;
-            }
+               for(int j=0; j<row; j++){
+                   if(attackable(row, i, j, column[j])){
+                       possible = false;
+                       break;
+                   }
+               }
+
+               if(possible){
+                   column[row] = i;
+                   rec_func(row+1);
+                   column[row] = 0;
+               }
+           }
 
         }
-        return false;
+
     }
 
     private static boolean attackable(int r1, int c1, int r2, int c2) {
-        if(c1 == c2) return true;
-        if(r1 - c1 == r2 -c2) return true;
-        if(r1 + c1 == r2 + c2) return true;
+
+        if(c1 == c2 || r1+c1 == r2+c2 || r1-c1 == r2-c2 ){
+            return true;
+        }
         return false;
     }
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         input();
-        // 1번째 원소부터 M번째 원소를 조건에 맞게 고르는 모든 방법을 탐색
-        rec_func(1);
-        System.out.println(ans);
+        rec_func(0);
+        System.out.println(result);
     }
 
     static class FastReader {

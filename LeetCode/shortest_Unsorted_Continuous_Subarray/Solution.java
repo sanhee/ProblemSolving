d1b7e8752a9d7e117b73e3d90cf9ca4f282798bf
sanhee <68936@naver.com>
2022-01-02 23:35:16 +0900

    [배열] Shortest Unsorted Continuous Subarray
    O(nlogN) 풀이
    
    - 배열을 비교할 때, 처음 달라지는 인덱스만 찾는 것이므로 `투포인터`를 활용해 비교함

---
+++ LeetCode/shortest_Unsorted_Continuous_Subarray/Solution.java
@@ -0,0 +1,47 @@
+package com.example.LeetCode.shortest_Unsorted_Continuous_Subarray;
+
+/*
+    1. nums 배열을 복사를 해서, 정렬을 합니다. (o(nlogn))
+    2. 비교를 합니다. (o(n))
+    3. 달라지는 부분부터 길이를 구합니다.
+*/
+
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+
+        if(nums.length == 1){
+            return 0;
+        }
+
+        int[] copy = new int[nums.length];
+
+        System.arraycopy(nums, 0, copy, 0, nums.length);
+
+        Arrays.sort(copy);
+
+        int sIdx = -1;
+        int eIdx = -1;
+
+        // 투 포인터
+        int right = nums.length -1;
+        for(int left=0; left<nums.length; left++){
+
+            // 종료조건 (중요)
+            if(right >= nums.length){
+                break;
+            }
+
+            if(sIdx == -1 && (nums[left] != copy[left])){
+                sIdx = left;
+            }
+
+            if(eIdx == -1 && (nums[right] != copy[right])){
+                eIdx = right;
+            }
+
+            right--;
+        }
+
+        return eIdx != -1 ? eIdx-sIdx+1 : 0;
+    }
+}
